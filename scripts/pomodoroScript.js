c4269ea25229e915a16b83d176d8098ab2ac2ec9
JHenderson219 <hendersonj219@gmail.com>
2016-11-23 16:43:41 -0800

    Design complete, working on functional buttons

--- scripts/pomodoroScript.js
+++ scripts/pomodoroScript.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
-
-
+var sessionLength = 25;
+var breakLength = 5;
+	function updateSessionScreen(){
+		$("#sessionScreen").empty().append("<h3 class = 'text-center'>"+sessionLength+"</h3>")
+	}
+	function updateBreakScreen(){
+		$("#breakScreen").empty().append("<h3 class = 'text-center'>"+sessionLength+"</h3>")
+	}
+	function reset(){
+		sessionLength = 25;
+		breakLength = 5;
+		updateSessionScreen();
+		updateBreakScreen();
+	}
+	$("#sessionMinus").click(function(){
+		sessionLength--;
+		updateSessionScreen()
+	});
+	$("#sessionPlus").click(function(){
+		sessionLength++;
+		updateSessionScreen();
+	});
+	$("#resetButton").click(function(){
+		reset();
+	});
+	
 });

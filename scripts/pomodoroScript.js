74b81f2fa4f31f9ae53c299d3b0aa9a11956275d
JHenderson219 <hendersonj219@gmail.com>
2016-12-09 10:56:38 -0800

    Alpha 0.2
    Added alert sound, refactored code, and added tap events

--- scripts/pomodoroScript.js
+++ scripts/pomodoroScript.js
@@ -7,12 +7,13 @@ var sessionLength = 25;
 //Length of a session; with default ratio (60000) this will represent minutes; ratio 1000 will represent seconds.
 var breakLength = 5;
 //Length of a break; with default ratio (60000) this will represent minutes; ratio 1000 will represent seconds.
-var timeRatio = 100;
+var timeRatio = 60000;
 //Multiplier applied to milisecond ratings
 var sessionAlertTimeout;
 var sessionCountDownInterval;
 var breakAlertTimeout;
 var breakCountDownTInterval;
+var beep = new Audio ("alert.wav");
 
 	function updateSessionScreen(){
 		$("#sessionScreen").empty().append("<h3 class = 'text-center'>"+sessionLength+"</h3>")
@@ -45,6 +46,7 @@ var breakCountDownTInterval;
 		updateBreakScreen();
 	}
 
+
 	function sessionOverAlert(duration){
 		sessionAlertTimeout=window.setTimeout(function(){
 			isSessionRunning=false;
@@ -54,11 +56,13 @@ var breakCountDownTInterval;
 			console.log ("Session over. Is break running? "+isBreakRunning);
 			console.log ("Session over. Is session ready? "+ isSessionRunning);
 			console.log ("Session over. Is break ready? "+breakReady);
+			beep.play();
 			alert("Session Complete! Time for a break of "+breakLength+" minutes. Press the start button below to begin.")
 			window.clearInterval(sessionCountDownInterval);
 			window.clearTimeout(sessionAlertTimeout);
 		},duration);
 	}
+
 	function countDownSession(){
 		console.log("Session has begun");
 		sessionCountDownInterval=window.setInterval(function(){
@@ -81,6 +85,7 @@ var breakCountDownTInterval;
 			reset();
 		},duration);
 	}
+
 	function countDownBreak(){
 		breakCountDownInterval=window.setInterval(function(){
 			breakLength--;
@@ -88,36 +93,8 @@ var breakCountDownTInterval;
 		},timeRatio);
 	}
 
-	$("#sessionMinus").click(function(){
-		if(sessionLength>1){
-		sessionLength--;
-		updateSessionScreen();
-		} else{
-			alert ("Can not decrese session time below 1 minute!");
-		}
-	});
-	$("#sessionPlus").click(function(){
-		sessionLength++;
-		updateSessionScreen();
-	});
-	$("#breakMinus").click(function(){
-		if(breakLength>1){
-		breakLength--;
-		updateBreakScreen()
-		} else{
-			alert("Can not decrese break length below 1 minute!");
-		}
-	});
-	$("#breakPlus").click(function(){
-		breakLength++;
-		updateBreakScreen();
-	});
-	$("#resetButton").click(function(){
-		console.log ("Timer was reset!");
-		reset();
-	});
 
-	$("#startButton").click(function(){
+	function startButton(){
 		console.log ("Start Button clicked. Is session running? "+isSessionRunning);
 		console.log ("Start button clicked. Is break running? "+isBreakRunning);
 		console.log ("Start button clicked. Is session ready? "+ sessionReady);
@@ -134,7 +111,76 @@ var breakCountDownTInterval;
 			breakOverAlert(breakTime);
 		} else if(isSessionRunning || isBreakRunning){
 			stopBreakOrSession();
+		}	
+	}
+
+	function sessionMinus(){
+		if(sessionLength>1){
+		sessionLength--;
+		updateSessionScreen();
+		} else{
+			alert ("Can not decrese session time below 1 minute!");
 		}
+	}
+
+	function sessionPlus(){
+		sessionLength++;
+		updateSessionScreen();		
+	}
+
+	function breakMinus(){
+		if(breakLength>1){
+			breakLength--;
+			updateBreakScreen()
+		} else{
+			alert("Can not decrese break length below 1 minute!");
+		}
+	}
+
+	function breakPlus(){
+		breakLength++;
+		updateBreakScreen();
+	}
+
+	//Click events for buttons
+	$("#sessionMinus").click(function(){
+		sessionMinus();
+	});
+	$("#sessionPlus").click(function(){
+		sessionPlus();
+	});
+	$("#breakMinus").click(function(){
+		breakMinus();
+	});
+	$("#breakPlus").click(function(){
+		breakPlus();
+	});
+	$("#resetButton").click(function(){
+		console.log ("Timer was reset!");
+		reset();
+	});
+	$("#startButton").click(function(){
+		startButton();
+	});
+	//Tap events for buttons
+	$("#sessionMinus").on("tap",function(){
+		sessionMinus();
+	});
+	$("#sessionPlus").on("tap",function(){
+		sessionPlus();
+	});
+	$("#breakMinus").on("tap",function(){
+		breakMinus();
+	});
+	$("#breakPlus").on("tap",function(){
+		breakPlus();
+	});
+	$("#resetButton").on("tap",function(){
+		console.log ("Timer was reset!");
+		reset();
+	});
+	$("#startButton").on("tap",function(){
+		startButton();
 	});
 	
 });
